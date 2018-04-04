3afba103dd696f71cc4b5c2a3a51681b7020d074
peterwurzinger <wurzingerp@gmail.com>
2018-04-04 15:26:50 +0200

    More Tests

---
+++ test/SamlOida.Test/Binding/EncodingHelperTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using SamlOida.Binding;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Xml;
+using Xunit;
+
+namespace SamlOida.Test.Binding
+{
+    public class EncodingHelperTest
+    {
+        private readonly SamlOptions _options;
+        private readonly XmlDocument _message;
+
+        public EncodingHelperTest()
+        {
+            _options = new SamlOptions();
+
+            _message = new XmlDocument();
+            var el = _message.CreateElement("Message");
+            _message.AppendChild(el);
+        }
+
+        [Fact]
+        public void EncodeShouldThrowExceptionOnUnknownEncoding()
+        {
+            Assert.Throws<NotImplementedException>(() =>
+                EncodingHelper.EncodeMessage(_options, "Unknown Encoding", new XmlDocument(), "SAMLRequest", null));
+        }
+
+        [Fact]
+        public void EncodeShouldPreserveRelayState()
+        {
+            const string relayState = "TestRelayState";
+
+            var querystring = EncodingHelper.EncodeMessage(_options, SamlAuthenticationDefaults.DeflateEncoding, _message, SamlAuthenticationDefaults.SamlRequestKey,
+                relayState);
+
+            Assert.True(querystring.HasValue);
+            var query = QueryHelpers.ParseQuery(querystring.ToString());
+
+            Assert.True(query.TryGetValue(SamlAuthenticationDefaults.RelayStateKey, out var actualRelayState));
+            Assert.Equal(relayState, WebUtility.UrlDecode(actualRelayState));
+        }
+
+        [Fact]
+        public void EncodeShouldAppendDeflatedMessageAsBase64()
+        {
+            var querystring = EncodingHelper.EncodeMessage(_options, SamlAuthenticationDefaults.DeflateEncoding, _message, SamlAuthenticationDefaults.SamlRequestKey,
+                null);
+
+            Assert.True(querystring.HasValue);
+            var query = QueryHelpers.ParseQuery(querystring.ToString());
+
+
+            Assert.True(query.TryGetValue(SamlAuthenticationDefaults.SamlRequestKey, out var actualSamlRequest));
+            var messageBytes = Convert.FromBase64String(WebUtility.UrlDecode(actualSamlRequest));
+
+            var actualMessage = new XmlDocument();
+            using (var memoryStream = new MemoryStream(messageBytes))
+            {
+                using (var deflateStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+                {
+                    actualMessage.Load(deflateStream);
+                }
+            }
+
+            Assert.Equal(_message, actualMessage);
+        }
+
+        [Fact]
+        public void EncodeShouldNotAppendRelayStateIfNull()
+        {
+            var querystring = EncodingHelper.EncodeMessage(_options, SamlAuthenticationDefaults.DeflateEncoding, _message, SamlAuthenticationDefaults.SamlRequestKey,
+                null);
+
+            Assert.True(querystring.HasValue);
+            var query = QueryHelpers.ParseQuery(querystring.ToString());
+
+            Assert.False(query.TryGetValue(SamlAuthenticationDefaults.RelayStateKey, out var _));
+        }
+
+        //TODO: Test Signature
+    }
+}
+
---
+++ test/SamlOida.Test/MessageHandler/XmlExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using SamlOida.MessageHandler;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+using Xunit;
+
+namespace SamlOida.Test.MessageHandler
+{
+    public class XmlExtensionsTests
+    {
+
+        [Fact]
+        public void RemoveSignatureShouldRemoveSignatures()
+        {
+            var xmlDoc = new XmlDocument();
+            var rootElement = xmlDoc.CreateElement("Message");
+            var signatureElement = xmlDoc.CreateElement("ds", "Signature", SignedXml.XmlDsigNamespaceUrl);
+            rootElement.AppendChild(signatureElement);
+            xmlDoc.AppendChild(rootElement);
+
+            xmlDoc.RemoveSignature();
+
+            Assert.Null(signatureElement.ParentNode); 
+            Assert.False(rootElement.HasChildNodes);
+        }
+    }
+}
