600c1a4ae4c2a7e215593b5c3745f88ebd5503c2
peterwurzinger <wurzingerp@gmail.com>
2018-04-19 17:38:00 +0200

    Test InResponseTo

--- test/SamlOida.Test/MessageHandler/MessageFactory/LogoutResponseFactoryTests.cs
+++ test/SamlOida.Test/MessageHandler/MessageFactory/LogoutResponseFactoryTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 using SamlOida.MessageHandler.MessageFactory;
 using SamlOida.Model;
 using Xunit;
@@ -17,11 +18,13 @@ namespace SamlOida.Test.MessageHandler.MessageFactory
         [Fact]
         public void ShouldCreateSuccessMessage()
         {
-            var options = new SamlOptions();
-            options.SignOutgoingMessages = false;
+            var options = new SamlOptions {SignOutgoingMessages = false};
 
-            var samlLogoutResponseMessage = new SamlLogoutResponseMessage();
-            samlLogoutResponseMessage.Success = true;
+            var samlLogoutResponseMessage = new SamlLogoutResponseMessage
+            {
+                Success = true,
+                InResponseTo = $"_{Guid.NewGuid():N}"
+            };
 
             var xmlDocument = _logoutResponseFactory.CreateMessage(options, samlLogoutResponseMessage);
 
@@ -33,7 +36,7 @@ namespace SamlOida.Test.MessageHandler.MessageFactory
             var issuerNode = xmlDocument.SelectSingleNode("/samlp:LogoutResponse/saml:Issuer", mgr);
             var statusNode = xmlDocument.SelectSingleNode("/samlp:LogoutResponse/samlp:Status", mgr);
             var statusCodeNode = xmlDocument.SelectSingleNode("/samlp:LogoutResponse/samlp:Status/samlp:StatusCode", mgr);
-
+            
             Assert.NotNull(logoutResponseNode);
             Assert.NotNull(issuerNode);
             Assert.NotNull(statusNode);
@@ -41,22 +44,21 @@ namespace SamlOida.Test.MessageHandler.MessageFactory
 
             Assert.Equal("", logoutResponseNode.Attributes["Destination"].Value);
             Assert.Equal("2.0", logoutResponseNode.Attributes["Version"].Value);
+            Assert.Equal(samlLogoutResponseMessage.InResponseTo, logoutResponseNode.Attributes["InResponseTo"]?.Value);
 
             Assert.Equal("urn:oasis:names:tc:SAML:2.0:status:Success", statusCodeNode.Attributes["Value"].Value);
         }
 
         [Fact]
-        public void ShouldCreateResponderMessage()
+        public void ShouldCreateFailureMessage()
         {
-            var options = new SamlOptions();
-            options.SignOutgoingMessages = false;
+            var options = new SamlOptions {SignOutgoingMessages = false};
 
-            var samlLogoutResponseMessage = new SamlLogoutResponseMessage();
-            samlLogoutResponseMessage.Success = false;
+            var samlLogoutResponseMessage = new SamlLogoutResponseMessage {Success = false};
 
             var xmlDocument = _logoutResponseFactory.CreateMessage(options, samlLogoutResponseMessage);
 
-            XmlNamespaceManager mgr = new XmlNamespaceManager(xmlDocument.NameTable);
+            var mgr = new XmlNamespaceManager(xmlDocument.NameTable);
             mgr.AddNamespace("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
             mgr.AddNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
 
