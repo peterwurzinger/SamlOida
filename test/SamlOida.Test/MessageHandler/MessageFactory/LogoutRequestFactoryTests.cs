8a443810c1891800a77b16ab4971104c1e50d947
Philipp Spindler <phi.spindler@gmail.com>
2018-04-19 17:41:55 +0200

    add signed Test to logoutrequestFactory

--- test/SamlOida.Test/MessageHandler/MessageFactory/LogoutRequestFactoryTests.cs
+++ test/SamlOida.Test/MessageHandler/MessageFactory/LogoutRequestFactoryTests.cs
@@ -1,5 +1,7 @@
 ﻿using SamlOida.MessageHandler.MessageFactory;
 using SamlOida.Model;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Xml;
 using Xunit;
 
@@ -20,6 +22,37 @@ namespace SamlOida.Test.MessageHandler.MessageFactory
             var options = new SamlOptions {SignOutgoingMessages = false};
 
             var authnRequestMessage = new SamlLogoutRequestMessage {NameId = "test"};
+
+            var xmlDocument = _logoutRequestFactory.CreateMessage(options, authnRequestMessage);
+
+            var mgr = new XmlNamespaceManager(xmlDocument.NameTable);
+            mgr.AddNamespace("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
+            mgr.AddNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
+
+            var logoutResponseNode = xmlDocument.SelectSingleNode("/samlp:LogoutRequest", mgr);
+            var issuerNode = xmlDocument.SelectSingleNode("/samlp:LogoutRequest/saml:Issuer", mgr);
+            var nameIDNode = xmlDocument.SelectSingleNode("/samlp:LogoutRequest/saml:NameID", mgr);
+            var sessionIndexNode = xmlDocument.SelectSingleNode("/samlp:LogoutRequest/samlp:SessionIndex", mgr);
+
+            Assert.NotNull(logoutResponseNode);
+            Assert.NotNull(issuerNode);
+            Assert.NotNull(nameIDNode);
+            Assert.NotNull(sessionIndexNode);
+
+            Assert.Equal("", logoutResponseNode.Attributes["Destination"].Value);
+            Assert.Equal("2.0", logoutResponseNode.Attributes["Version"].Value);
+
+            Assert.Equal("test", nameIDNode.InnerText);
+
+        }
+
+        [Fact]
+        public void ShouldCreateSignedMessage()
+        {
+            var privateCert = new X509Certificate2(File.ReadAllBytes("PrivateTestCert.pfx"), "test");
+            var options = new SamlOptions { SignOutgoingMessages = true, ServiceProviderCertificate = privateCert };
+
+            var authnRequestMessage = new SamlLogoutRequestMessage { NameId = "test" };
 
             var xmlDocument = _logoutRequestFactory.CreateMessage(options, authnRequestMessage);
 
